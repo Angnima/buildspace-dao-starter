73cb5c9aeec2f27fe047d1e81c8f9e1d39d3d5a6
Angnima Sherpa <angnimasherpa7@gmail.com>
2022-02-22 14:10:06 +0545

    Deploying ERC-20 Token contract for governance

--- scripts/5-deploy-token.js
+++ scripts/5-deploy-token.js
@@ -1,16 +1,18 @@
 import sdk from "./1-initialize-sdk.js";
 
 // In order to deploy the new contract we need our old friend the app module again.
-const app = sdk.getAppModule("INSERT_YOUR_APP_ADDRESS");
+const app = sdk.getAppModule("0xE9ad9F3BC88f782808B9f92D7FF50abae7C0C03d");
+
+// 0xaB959c4b8e42aFE3f3F4a250DFe2D50165383e55 BUIDL Token Address
 
 (async () => {
   try {
     // Deploy a standard ERC-20 contract.
     const tokenModule = await app.deployTokenModule({
       // What's your token's name? Ex. "Ethereum"
-      name: "NarutoDAO Governance Token",
+      name: "BuidlDAO Governance Token",
       // What's your token's symbol? Ex. "ETH"
-      symbol: "HOKAGE",
+      symbol: "BUIDL",
     });
     console.log(
       "✅ Successfully deployed token module, address:",
